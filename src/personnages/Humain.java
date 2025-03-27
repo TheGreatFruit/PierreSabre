fd16d4b858321dfda5d9b24b5ea4808c0b45859e
TheGreatFruit <pierre.28.gauthier@gmail.com>
2025-03-27 10:54:28 +0100

    creation de la classe humain

---
+++ src/personnages/Humain.java
@@ -0,0 +1,60 @@
+package personnages;
+
+public class Humain {
+	private String nom;
+	private String boissonFav;
+	private int qArgent;
+	
+	public Humain(String nom, String boissonFav, int qArgent) {
+		this.nom = nom;
+		this.boissonFav = boissonFav;
+		this.qArgent = qArgent;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public int getqArgent() {
+		return qArgent;
+	}
+	
+	public void parler(String texte) {
+		System.out.println(nom + " : " + texte + "\"");
+	}
+	
+	public void direBonjour() {
+		parler("Bonjour ! Je m'appelle " + nom + " et j'aime boire du "
+				+ boissonFav + ".");
+	}
+	
+	public void boire() {
+		parler("Mmmm, un bon verre de " + boissonFav + " ! GLOUPS !");
+	}
+	
+	public void acheter(String bien, int prix) {
+		if (qArgent >= prix) {
+			parler("J'ai " + qArgent + " sous en poche. "
+					+ "Je vais pouvoir m'offrir " + bien + " à " +
+					prix + " sous.");
+			perdreArgent(prix);
+		} else {
+			parler("Je n'ai plus que  " + qArgent +
+					" sous en poche. " + "Je ne peux même pas "
+							+ "m'offrir " + bien + " à " +
+					prix + " sous.");
+		}
+
+	}
+	
+	
+	private void gagnerArgent(int gain) {
+		qArgent += gain;
+	}
+	
+	
+	private void perdreArgent(int perte) {
+		qArgent -= perte;
+	}
+	
+}
