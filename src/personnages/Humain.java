522f4fc96c852425ce4464deddaa9c4cccff3ad0
TheGreatFruit <pierre.28.gauthier@gmail.com>
2025-04-03 11:19:44 +0200

    Ajout du systeme de memoire

--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -1,9 +1,14 @@
 package personnages;
 
+import java.util.Iterator;
+
 public class Humain {
 	private String nom;
 	private String boissonFav;
 	protected int qArgent;
+	protected Humain[] memoire = new Humain[30];
+	protected int nbConnaissance;
+
 	
 	public Humain(String nom, String boissonFav, int qArgent) {
 		this.nom = nom;
@@ -28,10 +33,60 @@ public class Humain {
 				+ boissonFav + ".");
 	}
 	
+	private void repondre() {
+		parler("Bonjour ! Je m'appelle " + nom + " et j'aime boire du "
+				+ boissonFav + ".");
+	}
+	
 	public void boire() {
 		parler("Mmmm, un bon verre de " + boissonFav + " ! GLOUPS !");
 	}
 	
+	public void faireConnaissanceAvec(Humain autreHumain) {
+		direBonjour();
+		autreHumain.repondre();
+		autreHumain.memoriser(this);
+		memoriser(autreHumain);
+		
+	}
+
+	private void memoriser(Humain humain) {
+	    // Vérifie si l'humain est déjà dans memoire
+	    for (int i = 0; i < nbConnaissance; i++) {
+	        if (memoire[i] == humain) {
+	            return; // L'humain est déjà mémorisé, donc on ne fait rien
+	        }
+	    }
+
+	    // Si le tableau est plein, on déplace les éléments vers la gauche et on ajoute la nouvelle connaissance
+	    if (nbConnaissance == memoire.length) {
+	        // Décale toutes les connaissances vers la gauche (écrase la première)
+	        for (int i = 1; i < nbConnaissance; i++) {
+	            memoire[i - 1] = memoire[i];  // Décale chaque élément vers la gauche
+	        }
+	    } else {
+	        // Si le tableau n'est pas encore plein, on décale juste une position et on ajoute la nouvelle connaissance
+	        nbConnaissance++;
+	    }
+
+	    // Ajoute la nouvelle connaissance à la dernière position (qui est maintenant disponible)
+	    memoire[nbConnaissance - 1] = humain;
+	}
+
+
+	
+	public void listerConnaissances() {
+		parler("Je connais beaucoup de monde dont : ");
+	    for (int i = 0; i < nbConnaissance; i++) {
+	        System.out.print(memoire[i].getNom());
+	        if (i < nbConnaissance - 1) {
+	            System.out.print(", ");
+	        }
+	    }
+	    System.out.println();
+	}	
+	
+
 	public void acheter(String bien, int prix) {
 		if (qArgent >= prix) {
 			parler("J'ai " + qArgent + " sous en poche. "
